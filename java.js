10a7a0f5460dcc09034e6367683aa947c29fea5b
KyanTG <kyan.gijtenbeek@hva.nl>
2024-10-24 12:32:51 +0200

    balbal

---
+++ java.js
@@ -0,0 +1,41 @@
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+/* JOUW CODE HIER - stap 4 */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+const openButton = document.querySelector("header > button");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+openButton.addEventListener("click", openMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {  
+  // zoek de nav op
+  const deNav = document.querySelector("nav");
+  // voeg class toe aan nav
+  deNav.classList.add("toonMenu");
+}
+
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+/* JOUW CODE HIER - stap 5 */
+
+// stap 1 - zoek sluiten button op
+const sluitButton = document.querySelector("nav button");
+
+// stap 2 - laat die button luisteren naar kliks
+sluitButton.addEventListener("click", sluitMenu);
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu() {
+  const deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
